485b3b88eaa588abbb98e46e7342a3323a3cfbbe
Gravitysause <57376726+Gravitysause@users.noreply.github.com>
2022-03-26 23:41:25 -0400

    Adding the Greatest Common Factor Algorithm
    This is a surprisingly inefficient way of finding the Greatest Common Factor in a list of numbers.

---
+++ Greatest_Common_Factor/Greatest_Common_Factor.py
@@ -0,0 +1,47 @@
+# Greatest Common Factors
+
+def is_prime(number):
+    if number > 1:
+  
+        # Iterate from 2 to n / 2
+        for i in range(2, int(number/2)+1):
+    
+            # If num is divisible by any number between
+            # 2 and n / 2, it is not prime
+            if (number % i) == 0:
+                break
+        else:
+            return True
+    
+    return False
+
+
+def check_factors(number):
+    factor_list = []
+
+    if is_prime(number):
+        factor_list += [1, number]
+        return factor_list
+
+    for length in range(1, number + 1, 1):
+        quotent = length    
+
+        if (number / quotent).is_integer():
+            factor_list += [quotent]
+
+    return factor_list
+
+
+def find_GCF(list_of_numbers):
+
+    all_factors = []
+    common_factors = []
+
+    for number in list_of_numbers:
+        all_factors += check_factors(number)
+
+    for number in all_factors:
+        if all_factors.count(number) == len(list_of_numbers):
+            common_factors += [number]
+
+    return max(common_factors)
